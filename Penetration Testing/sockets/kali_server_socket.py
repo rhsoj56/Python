f210e453b58a55ca5da0a0c28d3753d485116edd
Josh Rowell <rowelljosh6@gmail.com>
2023-05-16 20:37:05 +0100

    new folder, new scripts

---
+++ Penetration Testing/sockets/kali_server_socket.py
@@ -0,0 +1,17 @@
+import socket
+
+kali_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+port = 12345
+
+kali_server.bind(('', port)) #empty '' means the server will listen for ip
+
+kali_server.listen(2) #listen for connection
+print("Listening...")
+
+conn, addr = kali_server.accept()
+print(f"Connection from {addr}")
+
+conn.send("Thanks for connecting".encode())
+
+conn.close()
